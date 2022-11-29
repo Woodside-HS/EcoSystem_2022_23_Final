160efbcfa5f7335fe632e5e6d793b440ed5ae609
AdrianRWilson <114436469+AdrianRWilson@users.noreply.github.com>
2022-11-29 12:31:26 -0800

    grass (food5)

--- adrian/food5.js
+++ adrian/food5.js
@@ -2,34 +2,37 @@ class Food5 extends Entity {
     // properties
     constructor(loc, vel, sz, wrld) {
         super(loc, vel, sz, wrld)
-        
-    
+        this.loc = loc;
+        this.ctx = wrld.ctxMain;
+        this.size = sz;
+        this.wWidth = wrld.dims.width;
+        this.wHeight = wrld.dims.height;
+        this.statBlock = {
+        };
     }
-    //  methods
+
     run() {
-        this.update();
         this.render();
     }
 
-    update() {
-    }
-
     render() {
-       
+        let ctx = this.ctx;
+        ctx.save();
+        ctx.translate(this.loc.x, this.loc.y);
+        ctx.rotate(-Math.PI); //offset 90 degrees
+        ctx.beginPath();
+        ctx.strokeStyle = "#1dc714";
+        ctx.fillStyle = "#1dc714";
+        ctx.moveTo(-this.size, 0);
+        ctx.lineTo(this.size, 0);
+        ctx.lineTo(this.size, this.size);
+        ctx.lineTo(this.size/2, 0);
+        ctx.lineTo(0, this.size*2);
+        ctx.lineTo(-this.size/2, 0);
+        ctx.lineTo(-this.size, this.size);
+        ctx.closePath();
+        ctx.stroke();
+        ctx.fill();
+        ctx.restore();
     }
-
-    getRandomColor() {
-        //  List of hex color values for movers
-        let colors = [
-            "#25AA34",
-            "#18CC2e",
-            "#389925",
-            "#11AA99",
-            "#99CC00",
-            "#11FF65"
-        ];
-        let index = Math.floor(Math.random() * colors.length);
-        return colors[index];
-    }
-
 }
