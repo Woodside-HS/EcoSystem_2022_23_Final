e7e56d35eb7e83a7d137d1a254b4e29bf9d4d291
Tucker Gillbrand <810180@seq.org>
2022-12-01 12:07:39 -0800

    d
    final update, food items finished and done and dusted.

--- tucker/tFood2.js
+++ tucker/tFood2.js
@@ -10,6 +10,8 @@ class tFood2 extends Entity {
         this.foodParticleArray = [];
         //adds a beginner particle to kick off the array
         this.foodParticleArray.push(new tFood2P(this.loc, new JSVector(Math.random() * 1 - 0.5, Math.random() * 1 - 0.5), 5, this.ctx, Math.random()*20+20));
+        this.trunkClr = this.getRandomColor();
+        this.leaveClr = this.LgetRandomColor();
     }
     run() {
         this.update();
@@ -34,12 +36,43 @@ class tFood2 extends Entity {
     render() {
         //make a bush
         let ctx = this.ctx;
-        ctx.beginPath();
-        ctx.fillStyle = this.clr;
-        ctx.arc(this.loc.x, this.loc.y, this.rad, 0, Math.PI * 2);//I know cherries dont grow on bushed but I dont care
-        ctx.fill();
+        ctx.beginPath();//beginning of leafs
+        ctx.fillStyle = this.leaveClr;
+        ctx.arc(this.loc.x,this.loc.y-(this.rad*2),.75*this.rad,0,Math.PI*2);
+        ctx.arc(this.loc.x-this.rad,this.loc.y-(this.rad*2),this.rad*.5,0,Math.PI*2);
+        ctx.arc(this.loc.x+this.rad,this.loc.y-(this.rad*2),this.rad*.5,0,Math.PI*2);
+        ctx.closePath();
+        ctx.fill();//end of leaves
+        ctx.beginPath();//beginning of trunk
+        ctx.fillStyle = this.trunkClr;
+        ctx.moveTo(this.loc.x,this.loc.y);//center of trunk of tree
+        ctx.lineTo(this.loc.x+(this.rad/3),this.loc.y);
+        ctx.lineTo(this.loc.x+(this.rad/2),this.loc.y-this.rad);
+        ctx.lineTo(this.loc.x+this.rad,this.loc.y-(this.rad*2));
+        ctx.lineTo(this.loc.x,this.loc.y-(this.rad*1.5));
+        ctx.lineTo(this.loc.x+(this.rad/4),this.loc.y-(this.rad*2));
+        ctx.lineTo(this.loc.x,this.loc.y-(this.rad*1.75));//midpoint of tree
+        ctx.lineTo(this.loc.x-(this.rad*.25),this.loc.y-(this.rad*2));
+        ctx.lineTo(this.loc.x,this.loc.y-(this.rad*1.5));
+        ctx.lineTo(this.loc.x-this.rad,this.loc.y-(this.rad*2));
+        ctx.lineTo(this.loc.x-(this.rad*.5),this.loc.y-this.rad);
+        ctx.lineTo(this.loc.x-(this.rad/3),this.loc.y);
+        ctx.lineTo(this.loc.x,this.loc.y);//end of tree
+        ctx.closePath();
+        ctx.fill();//end of trunk
     }
     getRandomColor() {
+        //  List of hex color values for movers
+        let colors = [//array of various dark green bush colors
+            "#773700",
+            "#2F1600",
+            "#140900",
+            "#4D2400"
+        ];
+        let index = Math.floor(Math.random() * colors.length);
+        return colors[index];
+    }
+    LgetRandomColor() {
         //  List of hex color values for movers
         let colors = [//array of various dark green bush colors
             "#0F4800",
