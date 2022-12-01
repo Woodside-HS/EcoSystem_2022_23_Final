577ff29ce6ff5339c92d39b1385b6551c4bb6ed9
AdrianRWilson <114436469+AdrianRWilson@users.noreply.github.com>
2022-12-01 12:09:47 -0800

    food grows, lives, dies, and respawns

--- adrian/food5.js
+++ adrian/food5.js
@@ -4,15 +4,61 @@ class Food5 extends Entity {
         super(loc, vel, sz, wrld)
         this.loc = loc;
         this.ctx = wrld.ctxMain;
-        this.size = sz;
+        this.size = sz * 3/2;
         this.wWidth = wrld.dims.width;
         this.wHeight = wrld.dims.height;
         this.statBlock = {
         };
+        this.speedFactor = 5;
+        this.lifeSpan = getRandomInt(500/this.speedFactor, 1000/this.speedFactor);
+        this.life = this.lifeSpan;
+        this.isDead = false;
+        this.sizeFactor = this.life/this.lifeSpan;
+        this.growing = false;
+        this.livingSpan = getRandomInt(50/this.speedFactor, 1750/this.speedFactor);
+        this.living = false;
     }
 
     run() {
+        this.update();
         this.render();
+    }
+
+    update() {
+        if (this.growing) {
+            this.life++;
+            this.sizeFactor = this.life/this.lifeSpan;
+            if (this.life >= this.lifeSpan) {
+                this.growing = false;
+                this.living = true;
+                this.life = this.livingSpan;
+            }
+        } else if (this.living) {
+            this.life--;
+            this.sizeFactor = 1;
+            if (this.life <= 0) {
+                this.life = this.lifeSpan;
+                this.growing = false;
+                this.living = false;
+            }
+        } else {
+            this.life--;
+            this.sizeFactor = this.life/this.lifeSpan;
+            if (this.life <= 0) {
+                this.isDead = true;
+                this.respawn();
+            }
+        }
+    }
+
+    respawn() {
+        let x = Math.random() * (world.dims.width-20) - (world.dims.width / 2 - 10);
+        let y = Math.random() * (world.dims.height-20) - (world.dims.height / 2 - 10);
+        this.loc = new JSVector(x, y);
+        this.lifeSpan = getRandomInt(500/this.speedFactor, 1000/this.speedFactor);
+        this.life = 0;
+        this.isDead = false;
+        this.growing = true;
     }
 
     render() {
@@ -23,13 +69,13 @@ class Food5 extends Entity {
         ctx.beginPath();
         ctx.strokeStyle = "#1dc714";
         ctx.fillStyle = "#1dc714";
-        ctx.moveTo(-this.size, 0);
-        ctx.lineTo(this.size, 0);
-        ctx.lineTo(this.size, this.size);
-        ctx.lineTo(this.size/2, 0);
-        ctx.lineTo(0, this.size*2);
-        ctx.lineTo(-this.size/2, 0);
-        ctx.lineTo(-this.size, this.size);
+        ctx.moveTo(-this.size * this.sizeFactor, 0);
+        ctx.lineTo(this.size * this.sizeFactor, 0);
+        ctx.lineTo(this.size * this.sizeFactor, this.size * this.sizeFactor);
+        ctx.lineTo(this.size/2 * this.sizeFactor, 0);
+        ctx.lineTo(0, this.size*2 * this.sizeFactor);
+        ctx.lineTo(-this.size/2 * this.sizeFactor, 0);
+        ctx.lineTo(-this.size * this.sizeFactor, this.size * this.sizeFactor);
         ctx.closePath();
         ctx.stroke();
         ctx.fill();
