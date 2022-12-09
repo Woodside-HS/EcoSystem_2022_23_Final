2ae249048dd48eb6ac8b690c9db152e9fe1d77e9
MichaelSalmeron <806238@seq.org>
2022-12-09 01:48:56 -0800

    Start

---
+++ michael/MSfood2.js
@@ -0,0 +1,20 @@
+class MSFood1 extends Food {
+    // properties
+    constructor(loc, vel, sz, wrld) {
+        super(loc, vel, sz, wrld)
+        
+    
+    }
+    //  methods
+    run() {
+        this.update();
+        this.render();
+    }
+
+    update() {
+    }
+
+    render() {
+       
+    }
+}
