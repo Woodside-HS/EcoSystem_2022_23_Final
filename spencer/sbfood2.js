4395047165c452d146266080b38b19641f256a7b
Spencer Bergland <809083@seq.org>
2022-12-09 00:49:20 -0800

    Updated to work with creature

--- spencer/sbfood2.js
+++ spencer/sbfood2.js
@@ -1,46 +1,54 @@
-class SBFood2 extends Food {
+class SBFood2 extends Food {  //
     // properties
     constructor(loc, vel, sz, wrld) {
         super(loc, vel, sz, wrld);
         this.statBlock.health = 255-Math.random()*30;
-        this.lifespan = 0;
+        this.statBlock.lifeSpan = 0;
         this.clr = ["black", "rgba(39, 196, 220," + this.statBlock.health + ")", "rgba(255, 255, 255," + this.statBlock.health + ")"];
-        this.isDead = false;
         this.world = wrld;
         
     
     }
     //  methods
     run() {
-        if(!this.isDead){
+        console.log(this.statBlock.health);
         this.update();
         this.render();
-        }
     }
 
     update() {
         if(this.statBlock.health <= 1){
-            this.isDead = true;
-            this.loc = new JSVector(Math.random()*this.dims.width+this.dims.left, Math.random()*this.dims.height+this.dims.top);
-            this.dataBlock.health = 100;
-            this.dataBlock.nourishment = 100; 
+            let x = Math.random()*(world.dims.width-400)+world.dims.left+50;
+            let y = Math.random()*(world.dims.height-400)+world.dims.top+50;
+            this.loc = new JSVector(x, y);
+            this.statBlock.health = 255-Math.random()*30;
+            this.statBlock.nourishment = 100;
+            this.statBlock.lifeSpan = 0;
+            this.statBlock.opacity = 1.0;
+            this.statBlock.foodPts = 100;
+            
         }
-        if(this.lifespan%100 == 0){
+        if(this.statBlock.lifeSpan%100 == 0){
             this.statBlock.health--;
         }
-        this.lifespan++;
-        this.clr[1] = "rgba(39, 196, 220, " + this.statBlock.health + ")";
-        this.clr[2] = "rgba(255, 255, 255," + this.statBlock.health + ")";
+        this.statBlock.lifeSpan++;
     }
 
     render() {
         let ctx = this.ctx;
+        this.clr[1] = "rgba(39, 196, 220," + Math.floor(this.statBlock.health) + ")";
+        this.clr[2] = "rgba(255, 255, 255," + Math.floor(this.statBlock.health) + ")";
         for(let i = 1; i<=2; i++){
             ctx.beginPath();
             ctx.save();
             ctx.translate(this.loc.x, this.loc.y);
             ctx.rotate(2*i);
+            if(this.statBlock.health < 20){
+                ctx.fillStyle = this.clr[0];
+            }
+            else{
             ctx.fillStyle = this.clr[i];
+            }
             ctx.arc(0, 0, this.size, 0, Math.PI/2, false);
             ctx.closePath();
             ctx.fill();
@@ -51,7 +59,12 @@ class SBFood2 extends Food {
             ctx.save();
             ctx.translate(this.loc.x-(this.size)*(3/2)-1, this.loc.y-(this.size)*(3/2)+5.5);
             ctx.rotate(2*i+Math.PI);
+            if(this.statBlock.health < 20){
+                ctx.fillStyle = this.clr[0];
+            }
+            else{
             ctx.fillStyle = this.clr[i];
+            }
             ctx.arc(0, 0, this.size, 0, Math.PI/2, false);
             ctx.closePath();
             ctx.fill();
