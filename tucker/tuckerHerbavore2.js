79bfe188969a0d076d26b05ded9feef1e920d990
Tucker Gillbrand <112122417+810180@users.noreply.github.com>
2022-12-09 21:53:46 -0800

    1209 Progress on PS
    They are squares now and I made a bit of progress on the particle system searching. Have to figure out method to eat particle systems.

--- tucker/tuckerHerbavore2.js
+++ tucker/tuckerHerbavore2.js
@@ -1,6 +1,6 @@
-class tuckerHerbavore2 extends Creature{
+class tuckerHerbavore2 extends Creature {
     constructor(loc, vel, sz, wrld) {
-        super(loc,vel,sz,wrld)
+        super(loc, vel, sz, wrld)
         this.loc = loc;
         this.vel = vel;
         this.maxJump = 1;//the max acc's magnitude can be before it jumps
@@ -10,7 +10,7 @@ class tuckerHerbavore2 extends Creature{
         //various movement variable above
         this.ctx = wrld.ctxMain;
         this.foodEat = null;//which thing to eat
-        this.predatorsLocation = new JSVector(0,0);
+        this.predatorsLocation = new JSVector(0, 0);
         // this.statusBlock = { this is just for reference for me
         //     searchFood: true,
         //     searchMate: true,
@@ -40,6 +40,7 @@ class tuckerHerbavore2 extends Creature{
     run() {
         this.render();
         this.update();
+        //this.warpEdges(); doesnt work and I literally do not care
         //this.bounce(); this doesnt work and I dont care why
         // if(sprint){
 
@@ -54,33 +55,33 @@ class tuckerHerbavore2 extends Creature{
         }
 
     }
-    consuming(){
+    consuming() {
         let i = this.foodEat;
-            this.dataBlock.nourishment++;
-            if (world.foods.food2[i]) {//makes sure the food item still exists before you render it
-                if(world.foods.food2[i].statBlock.nourishment <= 2) {
-                    //this needs to go before the others so that it checks this forst
-                    //hopefully this should fix it -- It did not -- Wait it did
-                    world.foods.food2[i].statBlock.nourishment--;
-                    this.statusBlock.eating = false;
-                    this.statusBlock.searchFood = true;
-                    this.vel = new JSVector(Math.random() - 0.5, Math.random() - 0.5);
-                    this.foodEat = null;
-                } else if (world.foods.food2[i].statBlock.nourishment > 0) {//sometimes it just doens't exist - I think its cause of some splicng error
-                    world.foods.food2[i].statBlock.nourishment--;
-                } else {
-                    this.statusBlock.eating = false;
-                    this.statusBlock.searchFood = true;
-                    this.foodEat = null;
-                    this.vel = new JSVector(Math.random() - 0.5, Math.random() - 0.5);
-                }// I need some way to restart movement after eating something
-            } else {//should be checking to make sure that it goes back to check eat wont ever be run because it new thing is created
+        this.dataBlock.nourishment++;
+        if (world.foods.food2[i]) {//makes sure the food item still exists before you render it
+            if (world.foods.food2[i].statBlock.nourishment <= 2) {
+                //this needs to go before the others so that it checks this forst
+                //hopefully this should fix it -- It did not -- Wait it did
+                world.foods.food2[i].statBlock.nourishment--;
                 this.statusBlock.eating = false;
                 this.statusBlock.searchFood = true;
-                this.vel = new JSVector(Math.random() - 0.5, Math.random() - 0.5)
-            }
+                this.vel = new JSVector(Math.random() * 3 - 1.5, Math.random() * 3 - 1.5);
+                this.foodEat = null;
+            } else if (world.foods.food2[i].statBlock.nourishment > 0) {//sometimes it just doens't exist - I think its cause of some splicng error
+                world.foods.food2[i].statBlock.nourishment--;
+            } else {
+                this.statusBlock.eating = false;
+                this.statusBlock.searchFood = true;
+                this.foodEat = null;
+                this.vel = new JSVector(Math.random() * 3 - 1.5, Math.random() * 3 - 1.5);
+            }// I need some way to restart movement after eating something
+        } else {//should be checking to make sure that it goes back to check eat wont ever be run because it new thing is created
+            this.statusBlock.eating = false;
+            this.statusBlock.searchFood = true;
+            this.vel = new JSVector(Math.random() - 0.5, Math.random() - 0.5)
+        }
     }
-    searchingForFood(){//makes sure that the creature is actuawlly searching for food
+    searchingForFood() {//makes sure that the creature is actuawlly searching for food
         //normal foods below
         for (let i = 0; i < world.foods.food3.length; i++) {//only gos thru food3 rn should be pretty easily be able to modify to work with particle sytstems
             let sightSq = this.dataBlock.sightValue * this.dataBlock.sightValue;
@@ -88,7 +89,7 @@ class tuckerHerbavore2 extends Creature{
                 let jmp = JSVector.subGetNew(world.foods.food2[i].loc, this.loc);
                 jmp.setMagnitude(0.05);
                 this.acc.add(jmp);
-                if (this.loc.distanceSquared(world.foods.food2[i].loc) < 100) {//if the frog is close enough, then it will kill its velocity and end the search food and instead start eating
+                if (this.loc.distanceSquared(world.foods.food2[i].loc) < 400) {//if the frog is close enough, then it will kill its velocity and end the search food and instead start eating
                     this.vel.setMagnitude(0);
                     this.acc.setMagnitude(0);
                     this.statusBlock.searchFood = false;
@@ -98,10 +99,30 @@ class tuckerHerbavore2 extends Creature{
             }
         }
         //particle system below
-        for(let i = 0;i<world.foods.pSys2.length;i++){
-            let sightSq = this.dataBlock.sightValue * this.dataBlock.sightValue;
-            if(this.loc.distanceSquared(world.foods.pSys2[i].loc) < sightSq) {
-                //console.log("pSys spotted");
+        //IMPORTANT REMEMNBER THAT THIS IS A THING
+        let runPS = false;//test code so that I can see how it works with and without PS working
+        if (runPS) {
+            for (let i = 0; i < world.foods.pSys2.length; i++) {
+                let sightSq = this.dataBlock.sightValue * this.dataBlock.sightValue;
+                if (this.loc.distanceSquared(world.foods.pSys2[i].loc) < sightSq) {
+                    let pS = world.foods.pSys2[i].foodList;//goes directly to the food list
+                    if (pS.length > 1) {
+                        let maxNourish = pS[0].statBlock.nourishment;//will search for the one with the highest rotation so that it doesn't panic trying to go to every one
+                        for (let j = 0; j<pS.length; j++) {//this for loop breaks it - causes the page to go unresponsive and idk why
+                            let jmp = JSVector.subGetNew(pS[i].loc,this.loc);
+                            jmp.setMagnitude(0.05);
+                            this.acc.add(jmp);
+                            if (this.loc.distanceSquared(pS[i].loc) < 400) {//
+                                this.vel.setMagnitude(0);
+                                this.acc.setMagnitude(0);
+                                this.statusBlock.searchFood = false;
+                                this.statusBlock.eating = true;
+                                //this.foodEat = i;//sets foodEat to the # of the food variable to consume nourishment from to Do: Figure out how to eat the particle systems
+                            }
+                        }
+                    }
+
+                }
             }
         }
 
@@ -109,6 +130,20 @@ class tuckerHerbavore2 extends Creature{
     sprint(predLoc) {//predator will activate this it will send its current location to this creature every frame 
 
 
+    }
+    warpEdges() {
+        if (this.loc.x > world.dims.right) {
+            this.loc.x = world.dims.left;
+        }
+        if (this.loc.x < 0) {
+            this.loc.x = world.dims.width;
+        }
+        if (this.loc.y > world.dims.height) {
+            this.loc.y = 0;
+        }
+        if (this.loc.y < 0) {
+            this.loc.y = world.dims.height;
+        }
     }
     update() {
         if (this.acc.getMagnitude() > 1 && this.jmpCooldown >= this.jmpCooldownMax) {//makes sure there is already some momentum in the jump and that jump isnt on cooldown
@@ -129,24 +164,25 @@ class tuckerHerbavore2 extends Creature{
     render() {
         let ctx = this.ctx;
         ctx.beginPath();
+        ctx.strokeStyle = "blue";
         ctx.save();
-        ctx.translate(this.loc.x,this.loc.y);
-        //ctx.rotate(this.jmpCooldown);
-        ctx.moveTo(0,0);
-        ctx.lineTo(-this.rad,this.rad);
-        ctx.lineTo(this.rad,this.rad);
-        ctx.lineTo(this.rad,-this.rad);
-        ctx.lineTo(-this.rad,-this.rad);
+        ctx.translate(this.loc.x, this.loc.y);
+        ctx.rotate(this.acc.getDirection());
+        ctx.moveTo(0, 0);
+        ctx.lineTo(-this.rad, this.rad);//bottom left
+        ctx.lineTo(this.rad, this.rad);//bottom right
+        ctx.lineTo(this.rad, -this.rad);//top right
+        ctx.lineTo(-this.rad, -this.rad);//top left
+        ctx.lineTo(-this.rad, this.rad);
         ctx.restore();
-        ctx.closePath();//idunno why its spinning
+        ctx.closePath();//closes the square, to be fabulized at a later date of which is not confirmed yet.
         ctx.fill();
         ctx.beginPath();
+        ctx.strokeStyle = "red";
         ctx.moveTo(this.loc.x, this.loc.y);//this is the jump line so it is outside of the restore and rotation, is 4x the acc
         ctx.lineTo(this.loc.x + (this.acc.x * 4), this.loc.y + (this.acc.y * 4));
         ctx.fill();
-        ctx.closePath();
-        ctx.beginPath();
-        ctx.strokeStyle = "blue";
+        ctx.closePath();//closes just the line
         ctx.stroke();
     }
     bounce() {
