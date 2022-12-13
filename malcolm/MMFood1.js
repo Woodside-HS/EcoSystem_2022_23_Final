593d584c2aa83ddd2a569dee1a0f5ce14cba12d8
Malcolm McDonald <810828@seq.org>
2022-12-13 14:10:36 -0800

    starting the new food

---
+++ malcolm/MMFood1.js
@@ -0,0 +1,83 @@
+class MMFood1 extends Food {
+  // properties
+  constructor(loc, vel, sz, wrld) {
+    super(loc, vel, sz, wrld);
+    this.loc = loc;
+    this.vel = vel;
+    this.size = sz;
+    this.world = wrld;
+    this.ctxMain = wrld.ctxMain;
+    this.isDead = false;
+    this.count = 0;
+    this.tickRate = Math.random() * (150 - 50) + 50; //the tickrate of health decay
+    this.clr = "pink";
+    this.angle = Math.random() * (60 - 0);
+
+    // this.statBlock = {//  properties  //! del later
+    //     health: 100,
+    //     nourishment: 100,
+    //     lifeSpan:30000,
+    //     opacity:1.0,
+    //     foodPts:100
+    // };
+  }
+  //  methods
+  run() {
+    this.update();
+    this.render();
+    this.checkEdges();
+  }
+
+  update() {
+    if (this.statBlock.health <= 0) {
+      this.isDead = true;
+    }
+    // decays health
+    if (this.count++ >= this.tickRate) {
+      this.statBlock.health--;
+      this.count = 0;
+    }
+    this.loc.add(this.vel);
+  }
+
+  render() {
+    this.angle += 0.1;
+    this.ctxMain.save();
+    this.ctxMain.translate(this.loc.x, this.loc.y);
+    this.ctxMain.fillText(this.statBlock.health, -15, -15);
+    this.ctxMain.rotate(this.angle);
+    this.ctxMain.moveTo(0, -10);
+    this.ctxMain.lineTo(-10, -10);
+    this.ctxMain.lineTo(-10, 0);
+    this.ctxMain.lineTo(10, 0);
+    this.ctxMain.lineTo(10, 10);
+    this.ctxMain.lineTo(0, 10);
+    this.ctx.closePath();
+    this.ctxMain.fillStyle = this.clr;
+    this.ctxMain.strokeStyle = "black";
+    this.ctx.fill();
+    this.ctx.stroke();
+    this.ctx.restore();
+  }
+
+  getRandomColor() {
+    //  List of hex color values for movers
+    let colors = [
+      "#25AA34",
+      "#18CC2e",
+      "#389925",
+      "#11AA99",
+      "#99CC00",
+      "#11FF65",
+    ];
+    let index = Math.floor(Math.random() * colors.length);
+    return colors[index];
+  }
+  checkEdges() {
+    let dims = this.world.dims;
+    if (this.loc.x > dims.right) this.vel.x = -this.vel.x;
+    if (this.loc.x < dims.left) this.vel.x = -this.vel.x;
+    if (this.loc.y > dims.bottom) this.vel.y = -this.vel.y;
+    if (this.loc.y < dims.top) this.vel.y = -this.vel.y;
+  }
+}
--- world.js
+++ world.js
@@ -2,8 +2,8 @@
 class World {
   //  Commit 1: 221109
   constructor() {
-    this.cnvMain = document.getElementById('cnv1');
-    this.ctxMain = this.cnvMain.getContext('2d');
+    this.cnvMain = document.getElementById("cnv1");
+    this.ctxMain = this.cnvMain.getContext("2d");
     this.cnvMainLoc = new JSVector(0, 0);
     this.dims = {
       top: -1500,
@@ -11,8 +11,8 @@ class World {
       bottom: 1500,
       right: 2000,
       width: 4000,
-      height: 3000
-    }
+      height: 3000,
+    };
     this.showGrid = true;
     this.numRows = 90;
     this.numCols = 120;
@@ -22,7 +22,7 @@ class World {
     this.cnvMainRow = (this.cnvMainLoc.y - this.dims.top) / this.rowHeight;
     this.cnvMainCol = (this.cnvMainLoc.x - this.dims.left) / this.colWidth;
     this.rowRange = Math.floor(this.cnvMain.height / this.rowHeight);
-    this.colRange = Math.floor(this.cnvMain.width / this.colWidth);;
+    this.colRange = Math.floor(this.cnvMain.width / this.colWidth);
 
     this.grid = [];
     for (let row = 0; row < this.numRows; row++) {
@@ -54,7 +54,7 @@ class World {
       pSys3: [],
       pSys4: [],
       pSys5: [],
-      pSys6: []
+      pSys6: [],
     };
 
     //this.entities = [];
@@ -65,21 +65,18 @@ class World {
     this.framecount = 0;
     // every second (250 ms), see how many times that world.run() has
     // executed.
-    setInterval(
-      function () {
-        world.framerate = world.framecount;
-        world.framecount = 0;
-      },
-      1000
-    )
+    setInterval(function () {
+      world.framerate = world.framecount;
+      world.framecount = 0;
+    }, 1000);
   }
 
   run() {
     // performance
     this.framecount++;
     // run the world in animation
-    this.ctxMain.fillStyle = 'rgb(0, 0, 55)';//  color of outer border on Main canvas
-    this.ctxMain.clearRect(0, 0, this.cnvMain.width, this.cnvMain.height);//  clear the canvas
+    this.ctxMain.fillStyle = "rgb(0, 0, 55)"; //  color of outer border on Main canvas
+    this.ctxMain.clearRect(0, 0, this.cnvMain.width, this.cnvMain.height); //  clear the canvas
     // //+++++++++++++++++++++++++++ Draw all entites
     this.ctxMain.save();
     //  move the main canvas inside of the world
@@ -100,22 +97,35 @@ class World {
 
     // // translate cnvMain according to the location of the canvas in the world
     this.ctxMain.save();
-    this.ctxMain.translate(this.cnvMainLoc.x * (-1), this.cnvMainLoc.y * (-1));
+    this.ctxMain.translate(this.cnvMainLoc.x * -1, this.cnvMainLoc.y * -1);
     //bounds of the world in cnvMain
-    this.ctxMain.strokeStyle = "rgba(0, 140, 240, 1)"
+    this.ctxMain.strokeStyle = "rgba(0, 140, 240, 1)";
     this.ctxMain.beginPath();
     this.ctxMain.lineWidth = 12;
-    this.ctxMain.strokeRect(this.dims.left, this.dims.top, this.dims.width, this.dims.height);
+    this.ctxMain.strokeRect(
+      this.dims.left,
+      this.dims.top,
+      this.dims.width,
+      this.dims.height
+    );
     this.ctxMain.stroke();
     this.ctxMain.restore();
 
     // // performance  show framerate
     this.ctxMain.font = "20px  bold";
     this.ctxMain.fillStyle = "orange";
-    let fps = this.framerate + " FPS";  // frames per second
+    let fps = this.framerate + " FPS"; // frames per second
     this.ctxMain.fillText(fps, 20, this.cnvMain.height - 105);
-    this.ctxMain.fillText("Rows = " + this.numRows, 20, this.cnvMain.height - 130);
-    this.ctxMain.fillText("Cols = " + this.numCols, 20, this.cnvMain.height - 155);
+    this.ctxMain.fillText(
+      "Rows = " + this.numRows,
+      20,
+      this.cnvMain.height - 130
+    );
+    this.ctxMain.fillText(
+      "Cols = " + this.numCols,
+      20,
+      this.cnvMain.height - 155
+    );
     let numEnts = this.creatures.pred1.length + this.creatures.pred2.length;
     this.ctxMain.fillText("Ents = " + numEnts, 20, this.cnvMain.height - 85);
   }
@@ -123,15 +133,40 @@ class World {
   loadEntities(numEntities, ctx, w, h) {
     //  spencer Food 2
     for (let i = 0; i < numEntities; i++) {
-      this.foods.food2.push(new SBFood2(new JSVector(Math.random() * this.dims.width + this.dims.left, Math.random() * this.dims.height + this.dims.top), new JSVector(0, 0), 8, this))
+      this.foods.food2.push(
+        new SBFood2(
+          new JSVector(
+            Math.random() * this.dims.width + this.dims.left,
+            Math.random() * this.dims.height + this.dims.top
+          ),
+          new JSVector(0, 0),
+          8,
+          this
+        )
+      );
+    }
+
+    // Malcolm food 1
+    for (let i = 0; i < 50; i++) {
+      let x = Math.random() * this.cnvMain.width;
+      let y = Math.random() * this.cnvMain.height;
+      let loc = new JSVector(x, y);
+      let dx = Math.random() * 4 - 2;
+      let dy = Math.random() * 4 - 2;
+      let vel = new JSVector(dx, dy);
+      this.foods.food1.push(new MMFood1(loc, vel, 20, this));
     }
 
     // }//++++++++++++++++++++++++++++  load entities
     for (let i = 0; i < 500; i++) {
-      let x = Math.random() * (this.dims.width - 20) - (this.dims.width / 2 - 10);
-      let y = Math.random() * (this.dims.height - 20) - (this.dims.height / 2 - 10);
+      let x =
+        Math.random() * (this.dims.width - 20) - (this.dims.width / 2 - 10);
+      let y =
+        Math.random() * (this.dims.height - 20) - (this.dims.height / 2 - 10);
       let loc = new JSVector(x, y);
-      this.creatures.herb1.push(new Creature5(loc, new JSVector(0, 0), 6, this));//  Added to creatures object
+      this.creatures.herb1.push(
+        new Creature5(loc, new JSVector(0, 0), 6, this)
+      ); //  Added to creatures object
     }
 
     let c = this.creatures;
@@ -142,7 +177,7 @@ class World {
       let y = Math.random() * this.cnvMain.height;
       let loc = new JSVector(x, y);
       let dx = Math.random() * 4 - 2;
-      let dy = Math.random() * 4 - 2
+      let dy = Math.random() * 4 - 2;
       let vel = new JSVector(dx, dy);
       c.pred1.push(new Creature(loc, vel, 12, this));
     }
@@ -152,45 +187,41 @@ class World {
       let y = Math.random() * this.cnvMain.height;
       let loc = new JSVector(x, y);
       let dx = Math.random() * 4 - 2;
-      let dy = Math.random() * 4 - 2
+      let dy = Math.random() * 4 - 2;
       let vel = new JSVector(dx, dy);
       c.pred2.push(new Creature(loc, vel, 3, this));
     }
 
     for (let i = 0; i < numEntities; i++) {
-      let x = Math.random() * this.dims.width - (this.dims.width / 2);
-      let y = Math.random() * this.dims.height - (this.dims.height / 2);
+      let x = Math.random() * this.dims.width - this.dims.width / 2;
+      let y = Math.random() * this.dims.height - this.dims.height / 2;
       let loc = new JSVector(x, y);
       let dx = Math.random() * 4 - 2;
-      let dy = Math.random() * 4 - 2
+      let dy = Math.random() * 4 - 2;
       let vel = new JSVector(dx, dy);
       f.food2.push(new Food6(loc, vel, 5, this));
     }
     //  Tucker heart Food
     for (let i = 0; i < numEntities; i++) {
-      let x = Math.random() * this.dims.width - (this.dims.width / 2);
-      let y = Math.random() * this.dims.height - (this.dims.height / 2);
+      let x = Math.random() * this.dims.width - this.dims.width / 2;
+      let y = Math.random() * this.dims.height - this.dims.height / 2;
       let loc = new JSVector(x, y);
       let dx = Math.random() * 4 - 2;
-      let dy = Math.random() * 4 - 2
+      let dy = Math.random() * 4 - 2;
       let vel = new JSVector(dx, dy);
       f.pSys2.push(new tFood2(loc, vel, 7, this));
     }
     //  Adrain Food2Grass
     for (let i = 0; i < 50; i++) {
-      let x = Math.random() * this.dims.width - (this.dims.width / 2);
-      let y = Math.random() * this.dims.height - (this.dims.height / 2);
+      let x = Math.random() * this.dims.width - this.dims.width / 2;
+      let y = Math.random() * this.dims.height - this.dims.height / 2;
       let loc = new JSVector(x, y);
       let dx = Math.random() * 4 - 2;
-      let dy = Math.random() * 4 - 2
+      let dy = Math.random() * 4 - 2;
       let vel = new JSVector(dx, dy);
-      this.foods.food2.push(new Food2Grass(loc, vel, 7, this))
+      this.foods.food2.push(new Food2Grass(loc, vel, 7, this));
     }
-
   }
-
-
-
 
   runCreatures() {
     let c = this.creatures;
@@ -200,7 +231,6 @@ class World {
       if (c.pred1[i].dataBlock.isDead) {
         c.pred1.splice(i, 1);
       }
-
     }
 
     for (let i = c.pred2.length - 1; i >= 0; i--) {
@@ -209,9 +239,7 @@ class World {
         c.pred2.splice(i, 1);
       }
     }
-    for (let i = 0; i < c.pred3.length; i++) {
-
-    }
+    for (let i = 0; i < c.pred3.length; i++) {}
     for (let i = c.herb1.length - 1; i >= 0; i--) {
       c.herb1[i].run();
       if (c.herb1[i].dataBlock.isDead) {
@@ -219,19 +247,21 @@ class World {
       }
     }
 
-    for (let i = 0; i < c.herb2.length; i++) {
-
-    }
-    for (let i = 0; i < c.herb3.length; i++) {
-
-    }
-    for (let i = 0; i < c.flocks.length; i++) {
-
-    }
+    for (let i = 0; i < c.herb2.length; i++) {}
+    for (let i = 0; i < c.herb3.length; i++) {}
+    for (let i = 0; i < c.flocks.length; i++) {}
   }
 
   runFood() {
     let f = this.foods;
+
+    //! Malcolm food 1 run
+    for (let i = f.food1.length - 1; i >= 0; i--) {
+      f.food1[i].run();
+      if (f.food1[i].isDead) {
+        f.food1.splice(i, 1);
+      }
+    }
 
     //for (let i = c.food1.length - 1; i >= 0; i--) {
 
@@ -248,46 +278,26 @@ class World {
     //   this.foods.food2[i].run();
     // }
 
-    for (let i = f.food3.length - 1; i >= 0; i--) {
+    for (let i = f.food3.length - 1; i >= 0; i--) {}
 
-    }
+    for (let i = f.food4.length - 1; i >= 0; i--) {}
 
-    for (let i = f.food4.length - 1; i >= 0; i--) {
+    for (let i = f.food5.length - 1; i >= 0; i--) {}
 
-    }
+    for (let i = f.food6.length - 1; i >= 0; i--) {}
 
-    for (let i = f.food5.length - 1; i >= 0; i--) {
-
-    }
-
-    for (let i = f.food6.length - 1; i >= 0; i--) {
-
-    }
-
-    for (let i = f.pSys1.length - 1; i >= 0; i--) {
-
-    }
+    for (let i = f.pSys1.length - 1; i >= 0; i--) {}
 
     for (let i = f.pSys2.length - 1; i >= 0; i--) {
       f.pSys2[i].run();
     }
 
-    for (let i = f.pSys3.length - 1; i >= 0; i--) {
+    for (let i = f.pSys3.length - 1; i >= 0; i--) {}
 
-    }
+    for (let i = f.pSys4.length - 1; i >= 0; i--) {}
 
-    for (let i = f.pSys4.length - 1; i >= 0; i--) {
+    for (let i = f.pSys5.length - 1; i >= 0; i--) {}
 
-    }
-
-    for (let i = f.pSys5.length - 1; i >= 0; i--) {
-
-    }
-
-    for (let i = f.pSys6.length - 1; i >= 0; i--) {
-
-    }
+    for (let i = f.pSys6.length - 1; i >= 0; i--) {}
   }
-
-
-}//++++++++++++++++++++++++++++++  end world constructor
+} //++++++++++++++++++++++++++++++  end world constructor
