d72d7be455b43c588bab52c3d29362cf4d8f472f
Evan Demas <112122380+807116@users.noreply.github.com>
2022-12-14 12:34:31 -0800

    Update EvanHerbivore3.js

--- evan/EvanHerbivore3.js
+++ evan/EvanHerbivore3.js
@@ -27,7 +27,7 @@ class EvanHerbivore3 extends Creature {
           health: 100,
           isDead: false,
           nourishment: 100,
-          lifeSpan:30000,//  miliseconds
+          //lifeSpan:30000,//  miliseconds
           age:0,
           numOffspring:3,
           maxSpeed: 1,
@@ -40,11 +40,11 @@ class EvanHerbivore3 extends Creature {
  
     //++++++++++++++++++++++++++++++++ creature methods
     run() {
-       //console.log("hi");
+       
        this.update();
        this.checkEdges();
        this.render();
-       this.checkEating();   
+       this.searchingFood();   
     }
     update() {
        this.vel.add(this.acc);
@@ -66,22 +66,25 @@ class EvanHerbivore3 extends Creature {
        ctx.fillStyle = this.clr;
        ctx.arc(this.loc.x, this.loc.y, 20, 0, 2 * Math.PI, false);
        ctx.fill();
-       //  render balls in mini map
+       
     }
-    checkEating(){
+    searchingFood(){
       
       for(let i = 0; i<world.foods.food2.length; i++){
-         let d = this.loc.distance(world.foods.food2[i].loc);
+         let d = this.loc.distanceSquared(world.foods.food2[i].loc);
          if(d < 100){
-            this.acc = JSVector.subGetNew(world.foods.food2[i], this.loc);
-            this.acc.normalize();
-            this.acc.multiply(0.25);
+            let seek = JSVector.subGetNew(world.foods.food2[i].loc, this.loc);
+            seek.setMagnitude(0.05);
+            this.acc.add(seek);
+            if(this.loc.distanceSquared(world.foods.food2[i].loc) < 400){
+               this.vel.setMagnitude(0);
+                    this.acc.setMagnitude(0);
+                    this.statusBlock.searchFood = false;
+                    this.statusBlock.eating = true;
+                    this.foodEat = i;
+            }
          }
-         // if(this.loc.distance(world.foods.food2[i])<100){
-         //    v1 = JSVector.subGetNew(this.loc, world.foods.food2[i]);
-         //    this.vel.setDirection(world.foods.foods.food2[i]);
-            
-         // }
+
       }
         
     }
@@ -89,16 +92,8 @@ class EvanHerbivore3 extends Creature {
     getRandomColor() {
        //  List of hex color values for movers
        let colors = [
-          "#7102AB",
-          "#ab0256",
-          "#0285ab",
-          "#02ab1a",
-          "#ab5302",
-          "#773e26",
-          "#ab0256",
-          "#257874",
-          "#78254e",
-          "#787725"
+          "#7102AB"
+          
        ];
        let index = Math.floor(Math.random() * colors.length);
        return colors[index];
