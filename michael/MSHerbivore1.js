934facd0e20bd6a8469f0cb304ebfa6d5803b2bd
806238 <806238@seq.org>
2022-12-14 10:43:26 -0800

    rotating turtle

--- michael/MSHerbivore1.js
+++ michael/MSHerbivore1.js
@@ -22,21 +22,22 @@ class MSHerbivore1 extends Creature {
         this.ctx.beginPath();
         this.ctx.save();
         this.ctx.translate(this.loc.x,this.loc.y);
-        this.ctx.rotate(this.vel.getDirection());
-        this.ctx.ellipse(this.loc.x,this.loc.y-17,5,5,0,0,Math.PI*2);
+        this.ctx.rotate(this.vel.getDirection()+(Math.PI/2));
+        this.ctx.ellipse(0,-17,5,5,0,0,Math.PI*2);
         this.ctx.strokeStyle = "rgba(50,158,25,1)";
         this.ctx.fillStyle = "rgba(50,158,25,1)";
         this.ctx.stroke();
         this.ctx.fill();
         this.ctx.closePath();
         this.ctx.restore();
+        
 
         //feet
         this.ctx.beginPath();
         this.ctx.save();
         this.ctx.translate(this.loc.x,this.loc.y);
-        this.ctx.rotate(this.vel.getDirection());
-        this.ctx.ellipse(this.loc.x+10,this.loc.y+10,5,6,0,0,Math.PI*2);
+        this.ctx.rotate(this.vel.getDirection()+(Math.PI/2));
+        this.ctx.ellipse(10,10,5,6,0,0,Math.PI*2);
         this.ctx.strokeStyle = "rgba(50,158,25,1)";
         this.ctx.fillStyle = "rgba(50,158,25,1)";
         this.ctx.stroke();
@@ -47,8 +48,8 @@ class MSHerbivore1 extends Creature {
         this.ctx.beginPath();
         this.ctx.save();
         this.ctx.translate(this.loc.x,this.loc.y);
-        this.ctx.rotate(this.vel.getDirection());
-        this.ctx.ellipse(this.loc.x+10,this.loc.y-10,5,6,0,0,Math.PI*2);
+        this.ctx.rotate(this.vel.getDirection()+(Math.PI/2));
+        this.ctx.ellipse(10,-10,5,6,0,0,Math.PI*2);
         this.ctx.strokeStyle = "rgba(50,158,25,1)";
         this.ctx.fillStyle = "rgba(50,158,25,1)";
         this.ctx.stroke();
@@ -59,8 +60,8 @@ class MSHerbivore1 extends Creature {
         this.ctx.beginPath();
         this.ctx.save();
         this.ctx.translate(this.loc.x,this.loc.y);
-        this.ctx.rotate(this.vel.getDirection());
-        this.ctx.ellipse(this.loc.x-10,this.loc.y+10,5,6,0,0,Math.PI*2);
+        this.ctx.rotate(this.vel.getDirection()+(Math.PI/2));
+        this.ctx.ellipse(-10,10,5,6,0,0,Math.PI*2);
         this.ctx.strokeStyle = "rgba(50,158,25,1)";
         this.ctx.fillStyle = "rgba(50,158,25,1)";
         this.ctx.stroke();
@@ -71,8 +72,8 @@ class MSHerbivore1 extends Creature {
         this.ctx.beginPath();
         this.ctx.save();
         this.ctx.translate(this.loc.x,this.loc.y);
-        this.ctx.rotate(this.vel.getDirection());
-        this.ctx.ellipse(this.loc.x-10,this.loc.y-10,5,6,0,0,Math.PI*2);
+        this.ctx.rotate(this.vel.getDirection()+(Math.PI/2));
+        this.ctx.ellipse(-10,-10,5,6,0,0,Math.PI*2);
         this.ctx.strokeStyle = "rgba(50,158,25,1)";
         this.ctx.fillStyle = "rgba(50,158,25,1)";
         this.ctx.stroke();
@@ -81,24 +82,27 @@ class MSHerbivore1 extends Creature {
         this.ctx.restore();
 
         //shell
+        
+        
+        this.ctx.beginPath();
         this.ctx.save();
         this.ctx.translate(this.loc.x,this.loc.y);
-        this.ctx.rotate(this.vel.getDirection());
-        this.ctx.beginPath();
-        this.ctx.ellipse(this.loc.x,this.loc.y,10,15,0,0,Math.PI*2);
+        this.ctx.rotate(this.vel.getDirection()+(Math.PI/2));
+        this.ctx.ellipse(0,0,10,15,0,0,Math.PI*2);
         this.ctx.strokeStyle = "rgba(37,116,26,1)";
         this.ctx.fillStyle = "rgba(37,116,26,1)";
         this.ctx.stroke();
         this.ctx.fill();
         this.ctx.closePath();
         this.ctx.restore();
+        
 
         //eyes
         this.ctx.beginPath();
         this.ctx.save();
         this.ctx.translate(this.loc.x,this.loc.y);
-        this.ctx.rotate(this.vel.getDirection());
-        this.ctx.ellipse(this.loc.x+3,this.loc.y-20,1,1,0,0,Math.PI*2);
+        this.ctx.rotate(this.vel.getDirection()+(Math.PI/2));
+        this.ctx.ellipse(3,-20,1,1,0,0,Math.PI*2);
         this.ctx.strokeStyle = "rgba(0,0,0,1)";
         this.ctx.fillStyle = "rgba(0,0,0,1)";
         this.ctx.stroke();
@@ -106,49 +110,64 @@ class MSHerbivore1 extends Creature {
         this.ctx.closePath();
         this.ctx.restore();
         
+        
         this.ctx.beginPath();
         this.ctx.save();
         this.ctx.translate(this.loc.x,this.loc.y);
-        this.ctx.rotate(this.vel.getDirection());
-        this.ctx.ellipse(this.loc.x-3,this.loc.y-20,1,1,0,0,Math.PI*2);
+        this.ctx.rotate(this.vel.getDirection()+(Math.PI/2));
+        this.ctx.ellipse(-3,-20,1,1,0,0,Math.PI*2);
         this.ctx.strokeStyle = "rgba(0,0,0,1)";
         this.ctx.fillStyle = "rgba(0,0,0,1)";
         this.ctx.stroke();
         this.ctx.fill();
         this.ctx.closePath();
         this.ctx.restore();
+        
 
         //shell detail
         this.ctx.beginPath();
         this.ctx.save();
         this.ctx.translate(this.loc.x,this.loc.y);
-        this.ctx.rotate(this.vel.getDirection());
-        this.ctx.moveTo(this.loc.x,this.loc.y+7);
-        this.ctx.lineTo(this.loc.x,this.loc.y+15);
-        this.ctx.lineTo(this.loc.x,this.loc.y+7);//
-        this.ctx.lineTo(this.loc.x-5,this.loc.y+2);
-        this.ctx.lineTo(this.loc.x-10,this.loc.y+7);
-        this.ctx.lineTo(this.loc.x-5,this.loc.y+2);
-        this.ctx.lineTo(this.loc.x-5,this.loc.y-5);
-        this.ctx.lineTo(this.loc.x-9,this.loc.y-7);
-        this.ctx.lineTo(this.loc.x-5,this.loc.y-5);
-        this.ctx.lineTo(this.loc.x,this.loc.y-10);
-        this.ctx.lineTo(this.loc.x,this.loc.y-15);
-        this.ctx.lineTo(this.loc.x,this.loc.y-10);
-        this.ctx.lineTo(this.loc.x+5,this.loc.y-5);
-        this.ctx.lineTo(this.loc.x+9,this.loc.y-7);
-        this.ctx.lineTo(this.loc.x+5,this.loc.y-5);
-        this.ctx.lineTo(this.loc.x+5,this.loc.y+2);
-        this.ctx.lineTo(this.loc.x+10,this.loc.y+7);
-        this.ctx.lineTo(this.loc.x+5,this.loc.y+2);
-        this.ctx.lineTo(this.loc.x,this.loc.y+7);
+        this.ctx.rotate(this.vel.getDirection()+(Math.PI/2));
+        this.ctx.moveTo(0,7);
+        this.ctx.lineTo(0,15);
+        this.ctx.lineTo(0,0+7);//
+        this.ctx.lineTo(0-5,0+2);
+        this.ctx.lineTo(0-10,0+7);
+        this.ctx.lineTo(0-5,0+2);
+        this.ctx.lineTo(0-5,0-5);
+        this.ctx.lineTo(0-9,0-7);
+        this.ctx.lineTo(0-5,0-5);
+        this.ctx.lineTo(0,0-10);
+        this.ctx.lineTo(0,0-15);
+        this.ctx.lineTo(0,0-10);
+        this.ctx.lineTo(0+5,0-5);
+        this.ctx.lineTo(0+9,0-7);
+        this.ctx.lineTo(0+5,0-5);
+        this.ctx.lineTo(0+5,0+2);
+        this.ctx.lineTo(0+10,0+7);
+        this.ctx.lineTo(0+5,0+2);
+        this.ctx.lineTo(0,0+7);
         this.ctx.strokeStyle = "rgba(0,0,0,1)";
         this.ctx.stroke();
         this.ctx.closePath();
         this.ctx.restore();
+        
+        
     }
 
-    checkEdges(){
-       
-    }
+    checkEdges() {
+        if (this.loc.x < world.dims.left) {
+            this.vel.x = -this.vel.x;
+        }
+        if (this.loc.x > world.dims.right) {
+            this.vel.x = -this.vel.x;
+        }
+        if (this.loc.y > world.dims.bottom) {
+            this.vel.y = -this.vel.y;
+        }
+        if (this.loc.y < world.dims.top) {
+            this.vel.y = -this.vel.y
+        }
+      }
 }
