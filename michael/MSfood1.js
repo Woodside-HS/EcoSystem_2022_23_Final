127631ff469c621ca79554313d758c0c8c4c804d
806238 <806238@seq.org>
2022-12-20 12:11:03 -0800

    fix

--- michael/MSfood1.js
+++ michael/MSfood1.js
@@ -2,13 +2,8 @@ class MSFood1 extends Food {
     // properties
     constructor(loc, vel, sz, wrld) {
         super(loc, vel, sz, wrld)
+        this.loc = loc;
         this.statBlock.health = Math.random()*1+.05;
-        this.clr = "red"
-        this.clr2 = "green"
-        this.isDead = false;
-
-        
-    
     }
 
     //  methods
@@ -18,20 +13,40 @@ class MSFood1 extends Food {
     }
 
     update() {
+        //this.loc.add(this.vel);
         this.statBlock.health = this.statBlock.health-.0001;
+        if(this.statBlock.health >.1){
+            this.statBlock.nourishment = 100;
+            this.clr = "red"
+            this.clr2 = "green"
+            this.clr3 = "yellow"
+        }
         if(this.statBlock.health <= .1){
+            this.statBlock.nourishment = 50;
             this.clr = "rgb(86,57,14)"
             this.clr2 = "darkgreen"
+            this.clr3 = "black"
         }
 
         if(this.statBlock.health <= .03){
+            this.statBlock.nourishment = 25;
             this.clr = "black"
             this.clr2 = "rgb(86,57,14)"
+            this.clr3 = "black"
         }
 
         if(this.statBlock.health <= 0){
-            this.isDead = true;
+            this.statBlock.nourishment = 0;
+            this.isDead();
         }
+    }
+
+    isDead(){
+        let x = Math.random()*(world.dims.right-world.dims.left)+world.dims.left; 
+        let y = Math.random()*(world.dims.bottom-world.dims.top)+world.dims.top;
+        this.loc = new JSVector(x,y);
+        this.statBlock.health = Math.random()*1+.05;
+        this.statBlock.nourishment = 100;
     }
 
     render() {
@@ -54,10 +69,60 @@ class MSFood1 extends Food {
        this.ctx.lineTo(this.loc.x+5,this.loc.y-7);
        this.ctx.lineTo(this.loc.x,this.loc.y);
        this.ctx.strokeStyle = this.clr2;
-       this.ctx.lineWidth = 3;
+       //this.ctx.lineWidth = 3;
         this.ctx.stroke();
+       this.ctx.closePath();
+
+       this.ctx.beginPath();
+       this.ctx.ellipse(this.loc.x-6,this.loc.y+5,1,1,0,0,Math.PI*2);
+       this.ctx.strokeStyle = this.clr3;
+       this.ctx.fillStyle = this.clr3
+       this.ctx.fill();
+       this.ctx.stroke();
+       this.ctx.closePath();
+
+       this.ctx.beginPath();
+       this.ctx.ellipse(this.loc.x+6,this.loc.y+5,1,1,0,0,Math.PI*2);
+       this.ctx.strokeStyle = this.clr3;
+       this.ctx.fillStyle = this.clr3
+       this.ctx.fill();
+       this.ctx.stroke();
+       this.ctx.closePath();
+
+       this.ctx.beginPath();
+       this.ctx.ellipse(this.loc.x,this.loc.y+3,1,1,0,0,Math.PI*2);
+       this.ctx.strokeStyle = this.clr3;
+       this.ctx.fillStyle = this.clr3
+       this.ctx.fill();
+       this.ctx.stroke();
+       this.ctx.closePath();
+
+       this.ctx.beginPath();
+       this.ctx.ellipse(this.loc.x+3,this.loc.y+8,1,1,0,0,Math.PI*2);
+       this.ctx.strokeStyle = this.clr3;
+       this.ctx.fillStyle = this.clr3
+       this.ctx.fill();
+       this.ctx.stroke();
+       this.ctx.closePath();
+
+       this.ctx.beginPath();
+       this.ctx.ellipse(this.loc.x-3,this.loc.y+8,1,1,0,0,Math.PI*2);
+       this.ctx.strokeStyle = this.clr3;
+       this.ctx.fillStyle = this.clr3
+       this.ctx.fill();
+       this.ctx.stroke();
+       this.ctx.closePath();
+
+
+       this.ctx.beginPath();
+       this.ctx.ellipse(this.loc.x,this.loc.y+12,1,1,0,0,Math.PI*2);
+       this.ctx.strokeStyle = this.clr3;
+       this.ctx.fillStyle = this.clr3
+       this.ctx.fill();
+       this.ctx.stroke();
        this.ctx.closePath();
     }
 
     
 }
+
